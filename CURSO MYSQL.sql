5e0c6e5e2578c9d5c55f4a06e67d9031e40b1c7b
CassTi_ <cassiob@alfamaweb.com.br>
2023-01-30 13:24:31 -0300

    Descomenta códigos

--- CURSO MYSQL.sql
+++ CURSO MYSQL.sql
@@ -1,63 +1,63 @@
--- CRIAR BANCO: CREATE
--- DELETAR BANCO: DROP
--- USAR BANCO: USER
+ CRIAR BANCO: CREATE
+ DELETAR BANCO: DROP
+ USAR BANCO: USER
 
 
--- USE meubanco;
+ USE meubanco;
 
--- CREATE TABLE pessoa (
-	-- aqui dentro eu crio as colunas
---	nome VARCHAR(100)
--- );
+ CREATE TABLE pessoa (
+	 aqui dentro eu crio as colunas
+	nome VARCHAR(100)
+ );
 
--- DROP TABLE pessoa;
+ DROP TABLE pessoa;
 
--- TIPOS DE DADOS
--- DADOS PARA TEXTO, NUMERO E DATAS
--- TEXTO: varchar(1000) // devemos sempre	otimizar o espaço otimizar o tipo de dado na tabela. o mais utilizado é o varchar, pois limitamos o tamanho do dado desse campo.
--- NUMBERO: INT, BIG INT. Nós podemos limitar também uma determinada quantidade de caracteres. Quando não determinamos um limite ele salva no padrão (o limite máximo) o que pode ser um problema para o desempenho do banco
--- DATAS: os dois mais utilizados: TIMESTAMP(horas, segundos, mês e dias); DATE (anos, mês e dias)
--- EXEMPLO
+ TIPOS DE DADOS
+ DADOS PARA TEXTO, NUMERO E DATAS
+ TEXTO: varchar(1000) // devemos sempre	otimizar o espaço otimizar o tipo de dado na tabela. o mais utilizado é o varchar, pois limitamos o tamanho do dado desse campo.
+ NUMBERO: INT, BIG INT. Nós podemos limitar também uma determinada quantidade de caracteres. Quando não determinamos um limite ele salva no padrão (o limite máximo) o que pode ser um problema para o desempenho do banco
+ DATAS: os dois mais utilizados: TIMESTAMP(horas, segundos, mês e dias); DATE (anos, mês e dias)
+ EXEMPLO
 
 USE meubanco;
 
--- CREATE TABLE pessoas (
---	nome VARCHAR(100),
---	salario INT,
---	data_nascimento DATE
--- );
+ CREATE TABLE pessoas (
+	nome VARCHAR(100),
+	salario INT,
+	data_nascimento DATE
+ );
 
--- ALTERAR COLUNA
+ ALTERAR COLUNA
 
--- ALTER TABLE pessoas ADD COLUMN profissao VARCHAR(255);
+ ALTER TABLE pessoas ADD COLUMN profissao VARCHAR(255);
 
 
 
--- CRUD
--- C: create = CREATE = INSERT (toda inserção de tabela faz parte do create)
--- R: read = SELECT
--- U: update = UPDATE (alteração) - SEMPRE UTILIZE WHERE
--- D: delete = DELETE - SEMPRE UTILIZE WHERE
+ CRUD
+ C: create = CREATE = INSERT (toda inserção de tabela faz parte do create)
+ R: read = SELECT
+ U: update = UPDATE (alteração) - SEMPRE UTILIZE WHERE
+ D: delete = DELETE - SEMPRE UTILIZE WHERE
 
--- SELECT * FROM pessoas;
+ SELECT * FROM pessoas;
 
--- INSERIR DADOS
--- INSERT INTO pessoas (nome, salario, data_nascimento, profissao)
--- VALUES("Cássio", 3000, "2001-03-30", "programador");
+ INSERIR DADOS
+ INSERT INTO pessoas (nome, salario, data_nascimento, profissao)
+ VALUES("Cássio", 3000, "2001-03-30", "programador");
 
- -- SELECT * FROM pessoas WHERE data_nascimento = '0000-00-00';
+  SELECT * FROM pessoas WHERE data_nascimento = '0000-00-00';
  
--- INSERT INTO pessoas (nome, salario, data_nascimento, profissao) VALUES ("Milena N. Modesto", 8000, "2000-02-25", "Psicologa");
+ INSERT INTO pessoas (nome, salario, data_nascimento, profissao) VALUES ("Milena N. Modesto", 8000, "2000-02-25", "Psicologa");
  
--- SELECT * FROM pessoas;
+ SELECT * FROM pessoas;
 
--- ATUALIZAR DADOS
--- UPDATE pessoas SET salario = 3500 WHERE nome = "Cássio";
+ ATUALIZAR DADOS
+ UPDATE pessoas SET salario = 3500 WHERE nome = "Cássio";
 
--- DELETE FROM pessoas WHERE data_nascimento = "0000-00-00";
- -- SELECT * FROM pessoas;
+ DELETE FROM pessoas WHERE data_nascimento = "0000-00-00";
+  SELECT * FROM pessoas;
 
--- DELETE FROM pessoas WHERE nome = "Milena N. Modesto" AND salario > 2500;
+ DELETE FROM pessoas WHERE nome = "Milena N. Modesto" AND salario > 2500;
 
 SELECT * FROM pessoas;
 
--- FINALIZACAO.sql
+++ FINALIZACAO.sql
@@ -1,61 +1,61 @@
--- CONTRAINTS
+ CONTRAINTS
 
 
 
--- DROP TABLE pessoas;
--- SELECT * FROM pessoas;
--- ALTER TABLE pessoas ADD COLUMN profissao VARCHAR(100);
+ DROP TABLE pessoas;
+ SELECT * FROM pessoas;
+ ALTER TABLE pessoas ADD COLUMN profissao VARCHAR(100);
 
--- INSERT INTO pessoas (nome, salario, data_nascimento, profissao) VALUES ("Roberta", 6500, "1999-10-12", "Arquiteta de Software");
+ INSERT INTO pessoas (nome, salario, data_nascimento, profissao) VALUES ("Roberta", 6500, "1999-10-12", "Arquiteta de Software");
 
--- CONSTRAINTS - relação - FOREIGN KEY (chave estrangeira)
+ CONSTRAINTS - relação - FOREIGN KEY (chave estrangeira)
 
--- CREATE TABLE enderecos (
---	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
---	rua VARCHAR(255),
---	numero VARCHAR(10),
---	pessoa_id INT NOT NULL, -- chave estrangeira
---	FOREIGN KEY (pessoa_id) REFERENCES pessoas(id)
--- );
+ CREATE TABLE enderecos (
+	id INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	rua VARCHAR(255),
+	numero VARCHAR(10),
+	pessoa_id INT NOT NULL,  chave estrangeira
+	FOREIGN KEY (pessoa_id) REFERENCES pessoas(id)
+ );
 
--- INSERT INTO enderecos (rua, numero, pessoa_id) VALUES ("Rua das Árvores", "1314", 2);
--- SELECT * FROM pessoas;
-
-
--- JOIN
--- Consulta entre duas ou mais tabelas por meio de relações.
+ INSERT INTO enderecos (rua, numero, pessoa_id) VALUES ("Rua das Árvores", "1314", 2);
+ SELECT * FROM pessoas;
 
 
--- INNER JOIN
--- LEFT JOIN
--- RIGHT JOIN
-
--- INNER JOIN: SELECT pessoas.nome, enderecos.* FROM pessoas JOIN enderecos ON pessoas.id = enderecos.pessoa_id;
--- LEFT JOIN: SELECT pessoas.nome, enderecos.* FROM pessoas LEFT JOIN enderecos ON pessoas.id = enderecos.pessoa_id; trás os resultados que combinam + todos os itens da tabela esquerda
--- SELECT pessoas.nome, enderecos.* FROM pessoas RIGHT JOIN enderecos ON pessoas.id = enderecos.pessoa_id; trás os resultados que combinam, parecido com o INNER JOIN
+ JOIN
+ Consulta entre duas ou mais tabelas por meio de relações.
 
 
+ INNER JOIN
+ LEFT JOIN
+ RIGHT JOIN
+
+ INNER JOIN: SELECT pessoas.nome, enderecos.* FROM pessoas JOIN enderecos ON pessoas.id = enderecos.pessoa_id;
+ LEFT JOIN: SELECT pessoas.nome, enderecos.* FROM pessoas LEFT JOIN enderecos ON pessoas.id = enderecos.pessoa_id; trás os resultados que combinam + todos os itens da tabela esquerda
+ SELECT pessoas.nome, enderecos.* FROM pessoas RIGHT JOIN enderecos ON pessoas.id = enderecos.pessoa_id; trás os resultados que combinam, parecido com o INNER JOIN
 
 
--- AGREGATION FUNCTIONS
--- soma de todos os salários
 
--- SELECT SUM(salario) FROM pessoas;
 
--- ALIAS : nomeia determinada coluna
--- SELECT SUM(salario) AS soma_dos_salarios FROM pessoas;
+ AGREGATION FUNCTIONS
+ soma de todos os salários
 
--- COUNT: numero de registros de uma tabela - podemos especificamos mais ainda utilizando o where
--- SELECT COUNT(*) AS quantidade_de_pessoas FROM pessoas WHERE profissao = "Psicóloga";
+ SELECT SUM(salario) FROM pessoas;
 
--- GROUP BY: agrupa resultados por alguma condição
+ ALIAS : nomeia determinada coluna
+ SELECT SUM(salario) AS soma_dos_salarios FROM pessoas;
 
--- SELECT nome, profissao, COUNT(*) AS qutd_por_profissao FROM pessoas GROUP BY profissao;
+ COUNT: numero de registros de uma tabela - podemos especificamos mais ainda utilizando o where
+ SELECT COUNT(*) AS quantidade_de_pessoas FROM pessoas WHERE profissao = "Psicóloga";
 
--- FUNÇÕES DE TEXTO
+ GROUP BY: agrupa resultados por alguma condição
 
--- CONCATENAR TEXTO
--- SELECT CONCAT("O nome da pessoa é: ", nome, " e ele ganha R$: ", salario) AS descricao FROM pessoas;
+ SELECT nome, profissao, COUNT(*) AS qutd_por_profissao FROM pessoas GROUP BY profissao;
 
--- FUNÇÃO DE DATA
--- SELECT YEAR(data_nascimento)  AS ano_de_nascimento, MONTH(data_nascimento) AS mes_de_nascimento, DAY(data_nascimento) AS dia_de_nascimento FROM pessoas;
+ FUNÇÕES DE TEXTO
+
+ CONCATENAR TEXTO
+ SELECT CONCAT("O nome da pessoa é: ", nome, " e ele ganha R$: ", salario) AS descricao FROM pessoas;
+
+ FUNÇÃO DE DATA
+ SELECT YEAR(data_nascimento)  AS ano_de_nascimento, MONTH(data_nascimento) AS mes_de_nascimento, DAY(data_nascimento) AS dia_de_nascimento FROM pessoas;
